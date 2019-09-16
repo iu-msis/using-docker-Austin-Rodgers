6dd1fa06aa74bcfc3c707362683e133dd72ede09
Austin Rodgers <austinrodgers1451@yahoo.com>
2019-09-15 23:07:51 -0400

    Updated randomuser.js

--- app/public/js/RandomUser.js
+++ app/public/js/RandomUser.js
@@ -1,23 +1,67 @@
 var newUser = new Vue({
-  el: '#newUser',
-  data: {
-    user: {}
-  },
-  methods: {
-    fetchUser() {
-      fetch('https://randomuser.me/api')
-      .then(response => response.json())
-      .then(json => {newUser.user = json.results[0]});
-
-      // Means the same at this
-      // fetch('https://randomuser.me/api/')
-      // .then(function(response) {return response.json()})
-      // .then(function(json) {newUSer.users = json});
-
+      el: '#newUser',
+    data: {
+        User: {
+            "gender": "",
+            "name": {
+                "title": "",
+                "first": "",
+                "last": ""
+            },
+            "location": {
+                "street": "",
+                "city": "",
+                "state": "",
+                "postcode": "",
+                "coordinates": {
+                    "latitude": "",
+                    "longitude": ""
+                },
+                "timezone": {
+                    "offset": "",
+                    "description": ""
+                }
+            },
+            "email": "",
+            "login": {
+                "uuid": "",
+                "username": "",
+                "password": "",
+                "salt": "",
+                "md5": "",
+                "sha1": "",
+                "sha256": ""
+            },
+            "dob": {
+                "date": "",
+                "age": null
+            },
+            "registered": {
+                "date": "",
+                "age": null
+            },
+            "phone": "",
+            "cell": "",
+            "id": {
+                "name": "",
+                "value": ""
+            },
+            "picture": {
+                "large": "",
+                "medium": "",
+                "thumbnail": ""
+            },
+            "nat": ""
+        }
+    },
+    methods: {
+        fetchUser() {
+            fetch('https://randomuser.me/api')
+                .then(response => response.json())
+                .then(json => { randomUser.newUser = json.results[0] });
+        }
+    },
+    created: function () {
+        this.fetchUser();
     }
-  },
-  created(){
-    this.fetchUser();
-  }
-
 });
