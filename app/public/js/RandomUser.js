b35b089fcc87c541b8e79670089cc22dfd4c133d
Austin Rodgers <austinrodgers1451@yahoo.com>
2019-09-22 13:50:07 -0400

    Added code into js that will run when the "submit" button is clicked in user_info.html

--- app/public/js/RandomUser.js
+++ app/public/js/RandomUser.js
@@ -61,6 +61,62 @@ var newUser = new Vue({
                 .then(json => { newUser.user = json.results[0] });
         }
     },
+    loadNewUser() {
+           this.fetchUser();
+           this.user = {
+               "gender": "",
+               "name": {
+                   "title": "",
+                   "first": "",
+                   "last": ""
+               },
+               "location": {
+                   "street": "",
+                   "city": "",
+                   "state": "",
+                   "postcode": "",
+                   "coordinates": {
+                       "latitude": "",
+                       "longitude": ""
+                   },
+                   "timezone": {
+                       "offset": "",
+                       "description": ""
+                   }
+               },
+               "email": "",
+               "login": {
+                   "uuid": "",
+                   "username": "",
+                   "password": "",
+                   "salt": "",
+                   "md5": "",
+                   "sha1": "",
+                   "sha256": ""
+               },
+               "dob": {
+                   "date": "",
+                   "age": null
+               },
+               "registered": {
+                   "date": "",
+                   "age": null
+               },
+               "phone": "",
+               "cell": "",
+               "id": {
+                   "name": "",
+                   "value": ""
+               },
+               "picture": {
+                   "large": "",
+                   "medium": "",
+                   "thumbnail": ""
+               },
+               "nat": ""
+           };
+       }
+   },
     created: function () {
         this.fetchUser();
     }
